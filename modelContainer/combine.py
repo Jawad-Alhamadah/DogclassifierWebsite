6b9be9b588dc23dbe1a65149b9afde6d04d3553a
vortix2950 <vortix2950@ksu.edu>
2023-11-24 07:02:52 +0300

    sgs

--- modelContainer/combine.py
+++ modelContainer/combine.py
@@ -1,117 +1,200 @@
 
 import os
 import sys
-
-filename_flag ="--filename"
-#bytes_flag ="--size-in-bytes"
-num_chunks_flag ="--num-chunk-files"
-help_flag ="--help"
-keep_flag ="--keep-chunks"
-delete_chunks_flag ="--force-delete-chunks"
 from alive_progress import alive_bar;
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREY = '\033[90m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    HIGHLIGHT_GREY='\033[100m'
-    UNDERLINE = '\033[4m'
-
-bold = bcolors.BOLD
-cyan = bcolors.OKCYAN
-warning = bcolors.WARNING
-pink = bcolors.HEADER
-underline = bcolors.UNDERLINE
-green = bcolors.OKGREEN
-grey = bcolors.OKGREY
-end_color=bcolors.ENDC
-highlight_grey =bcolors.HIGHLIGHT_GREY
-fail=bcolors.FAIL
 
 
 
-if "-help" in sys.argv or help_flag in sys.argv :
-    print( f""" {bold}{pink}{underline}Enter the Following values with their respective Flags: {end_color}
-                1. {bold}{cyan}chunk combination Filename:  {grey}{ filename_flag }{end_color}
-                2. {bold}{warning}Number of chunks:  {grey}{ num_chunks_flag }{end_color}
-                3. {bold}{pink}-optional- {bold}{warning}intructions for script:  {grey}{help_flag}{end_color}
-                4. {bold}{pink}-optional- {bold}{warning}combine all chunks and keep the original chunks: {grey}{keep_flag}{end_color}
-                5. {bold}{pink}-optional- {bold}{warning}Remove Chunks without asking: {grey}{delete_chunks_flag}{end_color}
-    {bold}{highlight_grey}example:{end_color} python combine.py {cyan}{grey}{filename_flag}{end_color} myCopy.pt {warning} {grey}{num_chunks_flag}{end_color} 11
+KB_CONVERSION_UNIT = 1024
+MB_CONVERSION_UNIT = 1024 * 1024
+GB_CONVERSION_UNIT = 1024 * 1024 * 1024
+
+#* Colors for CMD messages
+fail='\033[91m'
+bold = '\033[1m'
+cyan = '\033[96m'
+warning = '\033[93m'
+pink = '\033[95m'
+underline = '\033[4m'
+green = '\033[92m'
+grey = '\033[90m'
+end_color='\033[0m'
+highlight_grey ='\033[100m'
+
+#* Function to get the value for arg flags
+def get_flag_value(flag,args):
+     flag_index = args.index(flag) + 1 
+     flag_value = args [flag_index]
+     return flag_value
+
+
+#*flags
+filename_flag = "--filename"
+num_chunks_flag = "--num-chunk-files"
+help_flag = "--help"
+keep_flag = "--keep-chunks"
+delete_chunks_flag = "--force-delete"
+extension_flag = "--ext"
+chunks_filename_flag = "--chunks-filename"
+default_chunk_name = "_temp-chunk.txt"
+chunks_filename = default_chunk_name
+
+script_args = [arg.lower() for arg in sys.argv]
+
+#* Help instructions.
+intro = f"""{bold}{pink}{underline}Enter the Following values with their respective Flags:{end_color}"""
+filename_instruction = f"""{bold}{cyan}chunk combination Filename:  {grey}{ filename_flag }{end_color}"""
+num_chunks_instruction =f"""{bold}{warning}Number of chunks:  {grey}{ num_chunks_flag }{end_color}"""
+ext_instruction = f"""{bold}{green}File extension : {grey} {extension_flag} {end_color}"""
+help_instruction =f"""{bold}{pink}-optional- {bold}{warning}intructions for script:  {grey}{help_flag}{end_color}"""
+keep_instruction =f"""{bold}{pink}-optional- {bold}{warning}combine all chunks and keep the original chunks: {grey}{keep_flag}{end_color}"""
+no_chunks_instruction =f"""{bold}{pink}-optional- {bold}{warning}Remove Chunks without asking: {grey}{delete_chunks_flag}{end_color}"""
+example_instruction = f"""{bold}{highlight_grey}example:{end_color} {end_color}{warning}python {end_color} combine.py {grey}{filename_flag}{end_color} {cyan}myCopy.pt {warning} {grey}{num_chunks_flag}{end_color} {green}11{end_color} """
+
+#* Error messages
+
+NO_FILENAME_FLAG_ERROR = f"{bold}{fail}No file name provided. Use {grey}{filename_flag}{fail} to name your result file. {end_color}"
+NUM_CHUNKS_ERROR = f"{bold}{fail}No chunk number provided. Use {grey}{num_chunks_flag}{fail} to indicate the number of chunks to sum. {end_color}"
+NO_EXT_ERROR = f"{bold}{fail}No extension included. Use {grey}{extension_flag}{fail} to set the extension. {end_color}"
+KEEP_AND_DELETE_ERROR = f"{grey}{keep_flag}{end_color}{warning} and {grey}{delete_chunks_flag}{warning} are contradictory flags. Remove one of them according to your needs. use {grey} --help {warning} to see options{end_color}"
+
+
+FIRST_FILE_NOT_EXIST = f"""{fail}{bold}ERROR: First chunk file is missing. Its likley that the chunk files don't exit, or inncorrect name provided to {grey}{chunks_filename_flag}{end_color}
+{fail}The default chunks name is {cyan}{default_chunk_name}{fail}. make sure the files exist or provide the correct chunks name {end_color}"""
+
+#* Warnings
+KEEP_DELETE_WARNING = f"""{bold}{warning}If you continue, data chunks will combine into a file and the chunks will be {fail}DELETED !{bold}{green} (exit and use flag {bold}{cyan}{keep_flag}{green} to combine and keep the chunks)
+{bold}{warning}  do you want to continue?{fail} y/n {end_color}"""
+
+
+
+#* error checking
+if filename_flag not in script_args:
+    print(NO_FILENAME_FLAG_ERROR)
+    sys.exit()
+
+if num_chunks_flag not in script_args:
+    print(NUM_CHUNKS_ERROR)
+    sys.exit()
+
+if extension_flag not in script_args:
+    print(NO_EXT_ERROR)
+    sys.exit()
+
+#* error if contradiction flag keep and delete are both raised
+is_delete_and_keep = keep_flag in script_args and delete_chunks_flag in script_args
+
+if is_delete_and_keep:
+    print(KEEP_AND_DELETE_ERROR)
+    sys.exit()
+
+combined_filename = get_flag_value( filename_flag, script_args )
+ext = get_flag_value( extension_flag, script_args )
+num_chunks = int( get_flag_value( num_chunks_flag, script_args ) )
+if chunks_filename_flag in script_args:
+    chunks_filename = get_flag_value( chunks_filename_flag, script_args ) 
+
+copy_ext = combined_filename.split(".")[1]
+
+
+EXTENSION_MISMATCH_ERROR = f"""{bold}{fail}\nThe copy name extenstion in filename: {cyan}{combined_filename} {fail}doesn't match flag extention {grey}{extension_flag} {cyan}{ext}{end_color} 
+{fail}{bold}Both must match to insure you combine the file into the correct extension. \n {end_color}"""
+if ext != copy_ext:
+     print(EXTENSION_MISMATCH_ERROR)
+     sys.exit()
+
+
+#* flag values.
+if "-help" in script_args or help_flag in script_args :
+    print( f""" \n{intro}\n
+                1. {filename_instruction}
+                2. {num_chunks_instruction}
+                3. {ext_instruction}
+                4. {help_instruction}
+                5. {keep_instruction}
+                6. {no_chunks_instruction}
+    \n{example_instruction}
           {end_color}""")
     sys.exit()
 
-if keep_flag in sys.argv and delete_chunks_flag in sys.argv:
-    print(f"{grey}{keep_flag}{end_color}{warning} and {grey}{delete_chunks_flag}{warning} are contradictory flags. Remove one of them according to your needs. use {grey} --help {warning} to see options{end_color}")
+
+delete_or_keep_user_input="yes"
+
+#* if neither keep or delete is raised, print a warning.
+is_neither_delete_or_keep = keep_flag not in script_args and delete_chunks_flag not in script_args
+if is_neither_delete_or_keep :
+    delete_or_keep_user_input = input(KEEP_DELETE_WARNING)
+
+#* if use answer is no, exist
+is_user_answer_no = delete_or_keep_user_input.lower() !="yes" and delete_or_keep_user_input !="y"
+if is_user_answer_no:
     sys.exit()
 
-#2. {bold}{green}chunk size in bytes:  {grey}{ bytes_flag }{end_color}
-
-
-user_cont_or_stop="yes"
-if keep_flag not in sys.argv and delete_chunks_flag not in sys.argv :
-    user_cont_or_stop = input(f"""{bold}{bcolors.WARNING}If you continue, data chunks will combine into a file and the chunks will be {bcolors.FAIL}DELETED !{bcolors.BOLD}{bcolors.OKGREEN} (exit and use flag {bcolors.BOLD}{bcolors.OKCYAN}{keep_flag}{bcolors.OKGREEN} to combine and keep the chunks)
-{bold}{bcolors.WARNING}  do you want to continue?{fail} y/n {bcolors.ENDC}""")
-
-
-if user_cont_or_stop.lower() !="yes" and user_cont_or_stop !="y":
-    sys.exit()
-
-
-index =1
-
-file_copy_name_index = sys.argv.index(filename_flag) + 1 
-file_copy_name = sys.argv[file_copy_name_index]
-
-#chunk_size_index = sys.argv.index(bytes_flag) + 1 
-#chunk_size = int( sys.argv[chunk_size_index] )
-
-num_segments_index = sys.argv.index(num_chunks_flag) + 1 
-num_segments = int( sys.argv[num_segments_index] )
-
-#
-file_common_name="-temp-chunk.txt"
-
-if "1"+file_common_name not in os.listdir():
-     print(f"{fail}{bold}ERROR: First chunk file is missing. Its likley that the chunk files don't exit.{end_color}")
+#* if the first file doesn't exist, print an error
+first_filename ="1"+chunks_filename
+if first_filename not in os.listdir():
+     print(FIRST_FILE_NOT_EXIST)
      sys.exit()
 
-count=0
+
+
+#* count the number of chunks to combine
+chunk_files_count=0
 list_dir= os.listdir()
 for i in range( len(list_dir) ):
-     if file_common_name in list_dir[i]:
-          count+=1
+     if chunks_filename in list_dir[i]:
+          chunk_files_count+=1
 
-if count > num_segments:
-    user_cont_or_stop = input(f"""{bold}{grey}\nText files with the token name :  {end_color}{file_common_name} {grey}are higher in number {cyan}({count} counted) {grey}than the inputted number of chunk files: {cyan}{num_segments}{grey} This will likely lead to some chunks being combined without reproducing the original file.
-\n{warning}do you want to continue regardless? {fail}y/n {green}(correcting the number is highly recommended){end_color}""")
+#* check the count provided against the count detected in the actual file.
+CHUNK_COUNT_MISMATCH = f"""{bold}{grey}\nText files with the token name :  {end_color}{chunks_filename} {grey}are higher in number {cyan}({chunk_files_count
+} counted) {grey}than the inputted number of chunk files: {cyan}{num_chunks}{grey} This will likely lead to some chunks being combined without reproducing the original file.
+\n{warning}do you want to continue regardless? {fail}y/n {green}(correcting the number is highly recommended){end_color}"""
+if chunk_files_count > num_chunks:
+    delete_or_keep_user_input = input(CHUNK_COUNT_MISMATCH)
 
-if user_cont_or_stop.lower() !="yes" and user_cont_or_stop !="y":
+#* if answer is no, print an error
+is_user_answer_no = delete_or_keep_user_input.lower() !="yes" and delete_or_keep_user_input !="y"
+if is_user_answer_no:
     sys.exit()
 
-chunk_size = os.path.getsize(str(1) + file_common_name)
+#* get chunks size
+chunk_size = os.path.getsize(str(1) + chunks_filename)
+byte_unit = "B"
+
+#* determine the unit for the byte size
+if chunk_size>=GB_CONVERSION_UNIT :
+    adjusted_chunk_size= chunk_size/GB_CONVERSION_UNIT
+    byte_unit= "Gb"
+
+elif chunk_size>=MB_CONVERSION_UNIT :
+    adjusted_chunk_size= chunk_size/MB_CONVERSION_UNIT
+    byte_unit="Mb"
+
+elif chunk_size>=KB_CONVERSION_UNIT :
+    adjusted_chunk_size= chunk_size/KB_CONVERSION_UNIT
+    byte_unit="Kb"
 
 
-model_copy = open( file_copy_name,mode="wb" )
+#* open file to write
+model_copy = open( combined_filename,mode="wb" )
 is_chunk_process_successful=True
 
 print("\n")
-with alive_bar(num_segments,bar="blocks",unit=" File",title="Combine",spinner="radioactive") as bar:
-    for i in range(1,num_segments+1):
+
+#* Loop to combine files into one.
+with alive_bar(num_chunks,bar="blocks",unit=" File",title="Combine",spinner="radioactive") as bar:
+    for i in range(1,num_chunks+1):
         try:
-            
-            chunk = open(str(i) + file_common_name, mode="rb")
+            #* read chunk
+            chunk = open(str(i) + chunks_filename, mode="rb")
             data = chunk.read(chunk_size)
+
+            #* write into combined file
             model_copy.write(data)
             bar()
         except FileNotFoundError:
-            print(f"""{bold}{fail}Some chunk files may be missing. Can't find file named: {grey}{str(i)+file_common_name}. {fail}Make sure that the value of {grey}--num-chunks{fail} flag is correct. 
+            print(f"""{bold}{fail}Some chunk files may be missing. Can't find file named: {grey}{str(i)+chunks_filename}. {fail}Make sure that the value of {grey}--num-chunks{fail} flag is correct. 
     its possible that the file was combined correctly but you should verify using fc.{end_color}""")
             is_chunk_process_successful=False
             break
@@ -120,23 +203,20 @@ with alive_bar(num_segments,bar="blocks",unit=" File",title="Combine",spinner="r
         
 model_copy.close()
 
-are_chunks_deleted= "Yes" if keep_flag not in sys.argv else "No"
+are_chunks_deleted= "Yes" if keep_flag not in script_args else "No"
 
-if keep_flag not in sys.argv and is_chunk_process_successful:
+if keep_flag not in script_args and is_chunk_process_successful:
     print(f"{green}\nFile Combined !\n{end_color}")
-    with alive_bar(num_segments,bar="blocks",unit=" File",title="Delete",spinner="arrows_in") as bar:
-        for i in range(1,num_segments+1):
-                    os.remove( str(i) + file_common_name)
+    with alive_bar(num_chunks,bar="blocks",unit=" File",title="Delete",spinner="arrows_in") as bar:
+        for i in range(1,num_chunks+1):
+                    os.remove( str(i) + chunks_filename)
                     bar()
 
 if is_chunk_process_successful:
      print(f"""\n{green}{bold}  * Files Combine Successful *{end_color}
-        {bold} {grey} File created: {cyan}{file_copy_name} {end_color}
-        {bold} {grey} Number of chunks combined : {cyan} {num_segments}{end_color}
+        {bold} {grey} File created: {cyan}{combined_filename} {end_color}
+        {bold} {grey} Number of chunks combined : {cyan} {num_chunks} Files{end_color}
         {bold} {grey} Were chunks deleted?  : {cyan} {are_chunks_deleted}{end_color}
+        {bold} {grey} Byte Size per chunk : {cyan} {adjusted_chunk_size} {byte_unit}  - {chunk_size} Bytes  
             {end_color}""")  
-
-
-    
-
 
--- modelContainer/split.py
+++ modelContainer/split.py
@@ -2,110 +2,177 @@ import sys
 import os
 import math
 from alive_progress import alive_bar;
-filename_flag ="--filename"
-bytes_flag ="--size-in-bytes"
-help_flag ="--help"
+
+#* Colors for CMD messages
+fail='\033[91m'
+bold = '\033[1m'
+cyan = '\033[96m'
+warning = '\033[93m'
+pink = '\033[95m'
+underline = '\033[4m'
+green = '\033[92m'
+grey = '\033[90m'
+end_color='\033[0m'
+highlight_grey ='\033[100m'
+
+
+
+#* one of these flags must exist. But only 1. if the size is bigger than 1, we throw an error
+size_flags_counter=0
+
+#* Flags
+FILENAME_FLAG = "--filename"
+BYTES_FLAG = "--size-bytes"
+KBYTES_FLAG = "--size-kilobytes"
+MBYTES_FLAG = "--size-megabytes"
+GBYTES_FLAG = "--size-gigabytes"
+EXT_FLAG = "--extention"
+HELP_FLAG ="--help"
+OUTPUT_FILENAME_FLAG ="--chunks-filename"
+
 KB_CONVERSION_UNIT=1024
-MB_CONVERSION_UNIT=1024000
+MB_CONVERSION_UNIT=1024 * 1024
+GB_CONVERSION_UNIT= 1024 * 1024 * 1024
+
+script_args = [arg.lower() for arg in sys.argv]
+def get_chunk_size(flag,conversion ):
+    chunk_size_index = script_args.index(flag) + 1 
+    chunk_byte_size = int( script_args[chunk_size_index] )
+    return chunk_byte_size * conversion
 
 
+if BYTES_FLAG in script_args:
+    # bytes_chosen_flag = BYTES_FLAG
+    # chunk_size_index = script_args.index(BYTES_FLAG) + 1 
+    chunk_byte_size = get_chunk_size(BYTES_FLAG,1)
+    size_flags_counter+=1
+
+if KBYTES_FLAG in script_args:
+    chunk_byte_size = get_chunk_size(KBYTES_FLAG,KB_CONVERSION_UNIT)
+    size_flags_counter+=1
+
+if MBYTES_FLAG in script_args:
+    chunk_byte_size = get_chunk_size(MBYTES_FLAG,MB_CONVERSION_UNIT)
+    size_flags_counter+=1
+
+if GBYTES_FLAG in script_args:
+    chunk_byte_size = get_chunk_size(GBYTES_FLAG,GB_CONVERSION_UNIT)
+    size_flags_counter+=1
+
+if OUTPUT_FILENAME_FLAG in script_args:
+    output_filename_index = script_args.index(OUTPUT_FILENAME_FLAG) + 1
+    chunks_filename = script_args[output_filename_index]
 
 
+filename_flag_instruction = f"""{bold}{cyan}Filename to split: {grey}{ FILENAME_FLAG }{end_color}{end_color}"""
+bytes_flag_instructions = f"""{bold}{green}segment size in bytes, KiloBytes, MegaBytes or GigaBytes: {grey}{BYTES_FLAG} {cyan}- {grey}{KBYTES_FLAG} {cyan}- {grey}{MBYTES_FLAG} {cyan}- {grey}{GBYTES_FLAG} {end_color}  """
+help_flag_instructions = f"""{bold}{pink}-optional- {bold}{warning}intructions for script:  {grey}{HELP_FLAG}{end_color}"""
+example_instruction = f""" {bold}{highlight_grey}example:{end_color} {warning}python {end_color}split.py {grey}{FILENAME_FLAG} {cyan}fileToSplit.pt {grey}{BYTES_FLAG} {green}50000000 """
+output_filename_instruction = f"""{bold}{pink}-optional- {warning} choose a name for output chunk files: {grey}{OUTPUT_FILENAME_FLAG}{end_color} """
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREY = '\033[90m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    HIGHLIGHT_GREY='\033[100m'
-    UNDERLINE = '\033[4m'
-
-fail=bcolors.FAIL
-bold = bcolors.BOLD
-cyan = bcolors.OKCYAN
-warning = bcolors.WARNING
-pink = bcolors.HEADER
-underline = bcolors.UNDERLINE
-green = bcolors.OKGREEN
-grey = bcolors.OKGREY
-end_color=bcolors.ENDC
-highlight_grey =bcolors.HIGHLIGHT_GREY
-
-
-if help_flag in sys.argv:
-    print( f"""{end_color} {bold}{pink}Enter the following in order : {end_color}
-                1. {bold}{cyan}Filename to split: {grey}{ filename_flag }{end_color}{end_color}
-                2. {bold}{green}segment size in bytes: {grey}{ bytes_flag }{end_color}{end_color}
-                3. {bold}{pink}-optional- {bold}{warning}intructions for script:  {grey}{help_flag}{end_color}
+#* text to display for when the help flag is raised
+HELP_INSTRUCTIONS =f"""\n{end_color}{underline}{bold}{pink}Enter the Following values with their respective Flags:\n {end_color}
+                1. {filename_flag_instruction}
+                2. {bytes_flag_instructions}
+                3. {output_filename_instruction}
+                4. {help_flag_instructions}
+                
                           
-        {bold}example:{end_color} python split.py {cyan}fileToSplit.pt {green}50000000 
-          {end_color}""")
+{example_instruction}
+          {end_color}"""
+
+
+#* display text when the file is not found
+
+NO_FILENAME_FLAG_ERROR = f"{bold}{fail}No file name provided. Use {grey}{FILENAME_FLAG}{fail} to pick the file to split {end_color}"
+
+NO_SIZE_ERROR = f"{bold}{fail}No size provided. Use {grey}--help {fail}to see avilable size flags{end_color}"
+
+MULTI_SIZE_ERROR= f"{bold}{fail}You can only have one {grey} --size {fail} flag at once. use {grey}--help{fail} for options{end_color}"
+
+file_name_counter=1
+
+#* default chunks name
+chunks_filename="_temp-chunk.txt" 
+
+
+#* error checking
+if HELP_FLAG in script_args:
+    print(HELP_INSTRUCTIONS)
+    sys.exit()
+
+if FILENAME_FLAG not in script_args:
+    print(NO_FILENAME_FLAG_ERROR)
+    sys.exit()
+
+    
+#* if size_flags_counter is 0, it means no chunks size was provided
+if size_flags_counter ==0:
+    print(NO_SIZE_ERROR)
+    sys.exit()
+
+if size_flags_counter >1:
+    print(MULTI_SIZE_ERROR)
     sys.exit()
 
 
-file_to_split_index = sys.argv.index(filename_flag) + 1 
-file_to_split = sys.argv[file_to_split_index]
-
-chunk_size_index = sys.argv.index(bytes_flag) + 1 
-chunk_size = int( sys.argv[chunk_size_index] )
 
 
-if not os.path.isfile(file_to_split):
-    print(f"{bold}{fail} ERROR: File named: {grey}{file_to_split} {fail}does not exist. {end_color}")
+file_to_split_index = script_args.index(FILENAME_FLAG) + 1 
+filename = script_args[file_to_split_index]
+
+#* check the size of the file to split. Then, calculate the number of resulting chunks
+file_size = os.stat(filename).st_size
+chunks_count = math.ceil(file_size/chunk_byte_size)
+
+
+#* use has the flag for a different name, change the default name
+
+FILE_NOT_FOUND_ERROR = f"{bold}{fail} ERROR: File named: {grey}{filename} {fail}does not exist. {end_color}"
+if not os.path.isfile(filename):
+    print(FILE_NOT_FOUND_ERROR)
     exit()
-#file_to_split = sys.argv[1]
-#chunk_size = int(sys.argv[2]) #50000000 #50 Mb
-#ext = file_to_split.split(".")[1]
 
-model_file = open(file_to_split,mode="rb")
-#data = model_file.read(chunk_size)
-files_list=[]
-file_name_counter=1
-file_common_name="-temp-chunk.txt"
+#* read the file
+model_file = open(filename,mode="rb")
 
-file_size = os.stat(file_to_split).st_size
-file_split_count = math.ceil(file_size/chunk_size)
 
-# import time
-# with alive_bar(5000,bar="smooth") as bar:
-#     for i in range(5000):
-#         time.sleep(.001)
-#         bar()
-with alive_bar(file_split_count,bar="blocks",unit=" File",title="Split") as bar:
-    for i in range(1,file_split_count+1) :
-        data = model_file.read(chunk_size)
-        file_name=str(file_name_counter)+file_common_name
+
+with alive_bar(chunks_count,bar="blocks",unit=" File",title="Split") as bar:
+    for i in range(1,chunks_count+1) :
+        data = model_file.read(chunk_byte_size)
+        file_name=str(file_name_counter)+chunks_filename
         file = open(file_name,mode="wb")
         file.write(data)
         file.close()
-        #files_list.append(file_name)
         file_name_counter+=1
         bar()
 
 
-unit= "Kb" if chunk_size>=KB_CONVERSION_UNIT else "B"
+byte_unit = 'B'
+adjusted_chunk_size = chunk_byte_size
 
-if chunk_size>=MB_CONVERSION_UNIT:
-    unit= "Mb"
 
-chunk_in_MB = 0
+#* convert to bytes to kb,mb,gb
+if chunk_byte_size >= GB_CONVERSION_UNIT :
+    adjusted_chunk_size = chunk_byte_size/GB_CONVERSION_UNIT
+    byte_unit = "Gb"
 
-if chunk_size>=KB_CONVERSION_UNIT and chunk_size< MB_CONVERSION_UNIT :
-    chunk_in_MB= chunk_size/KB_CONVERSION_UNIT
+elif chunk_byte_size >= MB_CONVERSION_UNIT :
+    adjusted_chunk_size = chunk_byte_size/MB_CONVERSION_UNIT
+    byte_unit = "Mb"
 
-if chunk_size>=MB_CONVERSION_UNIT :
-    chunk_in_MB= chunk_size/MB_CONVERSION_UNIT
+elif chunk_byte_size >= KB_CONVERSION_UNIT :
+    adjusted_chunk_size = chunk_byte_size/KB_CONVERSION_UNIT
+    byte_unit = "Kb"
 
+
+split_complete_message =f"""{green}{bold} \n  * File Split Successful *
+      {grey}    Number of chunks created : {cyan} {file_name_counter-1}
+      {grey}    Byte Size per chunk : {cyan} {adjusted_chunk_size} {byte_unit}  - {chunk_byte_size} Bytes  
+         {end_color}"""
 
 #
-print(f"""{green}{bold} \n  * File Split Successful *
-      {grey}    Number of chunks created : {cyan} {file_name_counter-1}
-      {grey}    Byte Size per chunk : {cyan} {chunk_in_MB} {unit}  - {chunk_size} Bytes  
-         {end_color}""")  
+print(split_complete_message)  
 model_file.close()
 
